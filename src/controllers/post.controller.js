b0e513342ac427b051da85d9baf77504a5c7ef4c
Arnanil Patra <arnanilpat2002@gmail.com>
2026-02-22 11:12:04 +0530

    Refactor token verification and update user field in post creation

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -21,19 +21,29 @@ async function createPostController(req,res){
         })
     }
 
-    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    let decoded= null ;
+
+         try{
+             decoded = jwt.verify(token, process.env.JWT_SECRET)
+         }catch(err){
+            return res.status(401).json({
+                message:"Invalid token,Unauthorized access"
+            })
+         }
+
 
     console.log(decoded) ;
     
     const file = await imagekit.files.upload({
         file : await toFile(Buffer.from(req.file.buffer), req.file.originalname),
         fileName : "Test",
+        folder:"instagram-clone-posts"
     })
 
     const post = await postmodel.create({
         caption:req.body.caption,
         imgUrl:file.url,
-        user:decoded.id
+        userId:decoded.id
     })
     
     res.status(201).json({
