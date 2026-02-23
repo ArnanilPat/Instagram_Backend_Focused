a4c17690fc4bfa1047af87d9e43787572549203e
Arnanil Patra <arnanilpat2002@gmail.com>
2026-02-23 10:45:35 +0530

    Implement user identification middleware and refactor post controllers to utilize it

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -3,19 +3,19 @@ const postRouter = express.Router();
 const postController = require("../controllers/post.controller");
 const multer = require("multer");
 const upload =  multer({storage:multer.memoryStorage()})
-
+const identifyUser = require("../middlewares/auth.middleware")
   
 
 
-postRouter.post("/",upload.single("image"),postController.createPostController)
+postRouter.post("/",upload.single("image"),identifyUser,postController.createPostController)
 
-postRouter.get("/",postController.getPostController)
+postRouter.get("/",identifyUser,postController.getPostController)
 
 /*
 *GET/api/posts/details/:postid
 rerurn details of a post with given postid.also check if the post belongs to the user or not
 */
 
-postRouter.get("/details/:postId",postController.getPostDetailsController)
+postRouter.get("/details/:postId",identifyUser,postController.getPostDetailsController)
 
 module.exports = postRouter;
